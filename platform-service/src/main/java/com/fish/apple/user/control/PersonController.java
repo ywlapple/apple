4d2d9e3bc883989759bdd0f84a449caa057975d2
ywlapple <ywlapple@163.com>
2018-07-15 12:38:40 +0800

    用户个人信息

--- platform-service/src/main/java/com/fish/apple/user/control/PersonController.java
+++ platform-service/src/main/java/com/fish/apple/user/control/PersonController.java
@@ -1,19 +1,24 @@
 package com.fish.apple.user.control;
 
-import javax.websocket.server.PathParam;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fish.apple.core.common.api.Response;
-import com.fish.apple.platform.bo.Account;
+import com.fish.apple.core.common.exception.BussinessException;
+import com.fish.apple.core.common.exception.Result;
 import com.fish.apple.platform.bo.Person;
-import com.fish.apple.platform.dict.AccountState;
 import com.fish.apple.user.repository.PersonRepository;
+import com.fish.apple.user.service.PersonService;
 
 @RestController
 @RequestMapping("/person")
@@ -21,44 +26,44 @@ public class PersonController {
 	
 	@Autowired
 	private PersonRepository repository ; 
+	@Autowired
+	private PersonService service ;
 	
-	private personservice
+	@RequestMapping(method = RequestMethod.GET ,path="/{id}" )
+	public Response<Person> getOne(@PathVariable Long id){
+		if(null == id ) {
+			throw BussinessException.create().kind(Result.IllegalArgument).msg("id").msg("id 不能为空");
+		}
+		Optional<Person> accountOp = repository.findById(id);
+		Response<Person> resp = new Response<>(); 
+		if(accountOp.isPresent()) {
+			return resp.success(accountOp.get());
+		}
+		return resp;
+	}
 	
-	@RequestMapping(method = RequestMethod.GET )
-	public Response<Person> save(@RequestBody Person person){
+	@RequestMapping(method = RequestMethod.GET ,path="/list" )
+	public Response<Page<Person>> getList(@RequestParam String accountNo , int size , int page){
+		Page<Person> findAll = repository.findAll(null, PageRequest.of(page, size));
+		Response<Page<Person>> resp = new Response<>();
+		return resp.success(findAll);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST )
+	public Response<Person> add(@RequestBody Person person){
 		Assert.notNull(person , "帐户信息不能为空");
 		Assert.notNull(person.getName() , "用户名称不能为空");
-		person = repository.saveAndFlush(person);
-		
+		person = service.save(person);
 		Response<Person> resp = new Response<>();
 		return resp.success(person);
 	}
-	
-	
-	@RequestMapping(method = RequestMethod.POST )
-	public Response<Account> save(@RequestBody Account account){
-		Assert.notNull(account , "帐户信息不能为空");
-		Assert.notNull(account.getLoginType() , "登录类型不能为空");
-		Assert.hasLength(account.getLoginName(), "登录名不能为空");
-		Assert.hasLength(account.getPassword(), "密码不能为空");
-		account.setAccountState(AccountState.init);
-		account = accountRepository.saveAndFlush(account);
-		Response<Account> resp = new Response<>();
-		return resp.success(account);
-	}
-	
 	@RequestMapping(method = RequestMethod.PUT )
-	public Response<Account> save(@RequestBody Person person){
+	public Response<Person> upd(@RequestBody Person person){
 		Assert.notNull(person , "用户信息不能为空");
-		Assert.notNull(account.getLoginType() , "登录类型不能为空");
-		Assert.hasLength(account.getLoginName(), "登录名不能为空");
-		Assert.hasLength(account.getPassword(), "密码不能为空");
-		account.setAccountState(AccountState.init);
-		account = accountRepository.saveAndFlush(account);
-		Response<Account> resp = new Response<>();
-		return resp.success(account);
-	}	
-	
-	
+		Assert.notNull(person.getId() , "Id不能为空");
+		person = service.save(person);
+		Response<Person> resp = new Response<>();
+		return resp.success(person);
+	}
 	
 }
